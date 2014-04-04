50132cce067aac834d2ede853eeb5bd3f70ade97
Lucas Hosseini <lucas.hosseini@ens-lyon.fr>
2014-04-04 21:00:03 +0200

    First on the scoreboard

--- vendredi/task.cpp
+++ vendredi/task.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
 
+const int COTE = 2;
+
 int N, M;
 vector<string> I;
 
@@ -26,20 +28,40 @@ print_img(const vector<string>& img) {
   }
 }
 
+inline bool
+check_square(int i, int j, int k) {
+  for (int l = -k; l <= k && i+l < N; ++l) {
+    for (int m = -k; m <= k && j+m < M; ++m) {
+      if (I[i+l][j+m] != '#')
+        return false;
+    }
+  }
+
+  return true;
+}
+
 void
 solve() {
-  vector< pair<int, int> > res;
-  rep(i, N) {
-    rep(j, M) {
-      if (I[i][j] == '#') {
-        res.push_back(make_pair(i, j));
+  vector< pair< pair<int, int>, int> > res;
+  for (int i = COTE; i < N; i += 2*COTE+1) {
+    for (int j = COTE; j < M; j+= 2*COTE+1) {
+      if (check_square(i, j, COTE)) {
+        res.push_back(make_pair(make_pair(i, j), COTE));
+      } else {
+        for (int l = -COTE; l <= COTE && i+l < N; ++l) {
+          for (int m = -COTE; m <= COTE && j+m < M; ++m) {
+            if (I[i+l][j+m] == '#') {
+              res.push_back(make_pair(make_pair(i+l, j+m), 0));
+            }
+          }
+        }
       }
     }
   }
 
   cout << res.size() << endl;
   rep(i, res.size()) {
-    cout << "PAINTSQ " << res[i].first << " " << res[i].second << " 0" << endl;
+    cout << "PAINTSQ " << res[i].first.first << " " << res[i].first.second << " " << res[i].second << endl;
   }
 }
 
