e0c958032ae7490dbec2c0d172711b93536a3d8c
Lucas Hosseini <lucas.hosseini@ens-lyon.fr>
2014-04-05 12:42:45 +0200

    Seed optim.

--- samedi/random_walk.cpp
+++ samedi/random_walk.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <set>
 
 using namespace std;
 
@@ -31,7 +32,7 @@ int N, M, T, C, S;
 vector< vector<int> > Adj;
 vector<Vertex> V;
 vector<Edge> E;
-vector< vector<int> > Solution;
+vector< vector< pair<int, int> > > Solution;
 
 inline double 
 gain(int car, int e, int t) {
@@ -83,18 +84,18 @@ endpoint(int v, int e) {
 }
 
 inline void
-traverse(int car, vector<int>& solution) {
+traverse(int car, vector< pair<int, int> >& solution) {
   int pos = S;
   int t = T;
   int next_edge = -1;
 
-  solution.push_back(S);
+  solution.push_back(make_pair(S, -1));
 
   while ((next_edge = chose_edge(car, pos, t)) != -1) {
     E[next_edge].traversed = true;
     t -= E[next_edge].C;
     pos = endpoint(pos, next_edge);
-    solution.push_back(pos);
+    solution.push_back(make_pair(pos, next_edge));
   }
 }
 
@@ -137,20 +138,57 @@ print_output() {
   rep(i, C) {
     cout << Solution[i].size() << endl;
     rep(j, Solution[i].size()) {
-      cout << Solution[i][j] << endl;
+      cout << Solution[i][j].first << endl;
     }
   }
 }
 
+inline void
+clear() {
+  rep (i, C) {
+    Solution[i].clear();
+  }
+  rep (i, M) {
+    E[i].traversed = false;
+  }
+} 
+
+inline int
+score() {
+  set<int> edges;
+  int res = 0;
+  rep(i, C) {
+    rep(j, Solution[i].size()) {
+      if (j == 0) continue;
+      edges.insert(Solution[i][j].second);
+    }
+  }
+
+  for (set<int>::iterator it = edges.begin(); it != edges.end(); ++it) {
+    res += E[*it].L;
+  }
+
+  return res;
+}
+
 int
 main() {
-  srand(time(NULL));
-
   read_input();
 
-  solve();
+  int best_score = 0;
 
-  print_output();
+  rep (i, 1000000) {
+    srand(i);
+    solve();
+    int s = score();
+    if (s > best_score) {
+      cout << s << " (" << i << ")" << endl;
+      best_score = s;
+    }
+    clear();
+  }
+
+  //  print_output();
 
   return 0;
 }
